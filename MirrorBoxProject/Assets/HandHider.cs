f7f723cea395fe38572c011f285241666e91ef01
yams <daniphye@gmail.com>
2017-06-02 11:41:38 -0600

    Added code and exe

---
+++ MirrorBoxProject/Assets/HandHider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandHider : MonoBehaviour {
+
+    public Transform[] handsToHide;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (Transform handToHide in handsToHide)
+        {
+		    foreach (Renderer renderer in handToHide.GetComponentsInChildren<Renderer>())
+            {
+                renderer.enabled = false;
+            }
+        }
+    }
+}
